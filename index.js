599eb9fe9619c5013ecc045e699659e297dc6581
rajesh.donepudi <rajesh.donepudi@techigai.io>
2022-10-13 11:05:34 +0530

    code refactor

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
   let dragableItemsContainer = document.querySelector(
     ".dragable-elements-container"
   );
-
   let selectedDropEffect = document.querySelector(".select-drop-effect");
-
   let droppableContainer = document.querySelector(".droppable-container");
 
   for (let index = 0; index < 8; index++) {
@@ -21,13 +19,12 @@
   draggableFields.forEach((element) => {
     element.addEventListener("dragstart", (event) => {
       event.dataTransfer.setData("application/my-app", event.target.id);
-      // event.dataTransfer.effectAllowed = "move";
       event.dataTransfer.effectAllowed = selectedDropEffect.value;
-
       event.target.classList.add("draggable-field-drag-effect");
 
       var dragImage = new Image();
       dragImage.src = "https://cdn-icons-png.flaticon.com/512/636/636045.png";
+
       event.dataTransfer.setDragImage(dragImage, 5, 5);
     });
   });
